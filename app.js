a130fe28ed5e52fa0316845669ceba6c70b09ad0
AndySkr <542842620@qq.com>
2019-11-05 23:54:01 +0800

    中间件测试

--- app.js
+++ app.js
@@ -1,56 +1,79 @@
-const Koa = require('koa')
-const app = new Koa()
-const views = require('koa-views')
-const json = require('koa-json')
-const onerror = require('koa-onerror')
-const bodyparser = require('koa-bodyparser')
-const logger = require('koa-logger')
-const cors = require('koa2-cors')
+const Koa = require('koa');
+const app = new Koa();
+const views = require('koa-views');
+const json = require('koa-json');
+const onerror = require('koa-onerror');
+const bodyparser = require('koa-bodyparser');
+const logger = require('koa-logger');
+const cors = require('koa2-cors');
 
-const index = require('./routes/index')
+const index = require('./routes/index');
 // const users = require('./routes/users')
 
 // error handler
-onerror(app)
+onerror(app);
 
 // middlewares
-app.use(bodyparser({
-  enableTypes: ['json', 'form', 'text']
-}))
-app.use(json())
-app.use(logger())
+app.use(
+    bodyparser({
+        enableTypes: ['json', 'form', 'text']
+    })
+);
+app.use(json());
+app.use(logger());
 app.use(require('koa-static')(__dirname + '/public')); //配置静态文件夹地址
 
-app.use(views(__dirname + '/views', {
-  extension: 'pug'
-}))
+app.use(
+    views(__dirname + '/views', {
+        extension: 'pug'
+    })
+);
 //跨域
-app.use(cors({
-  origin: function (ctx) {
-    return 'http://localhost:3000';
-  },
-  exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
-  maxAge: 5,
-  credentials: true,
-  allowMethods: ['GET', 'POST', 'DELETE'],
-  allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
-}))
+app.use(
+    cors({
+        origin: function(ctx) {
+            return 'http://localhost:3000';
+        },
+        exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
+        maxAge: 5,
+        credentials: true,
+        allowMethods: ['GET', 'POST', 'DELETE'],
+        allowHeaders: ['Content-Type', 'Authorization', 'Accept']
+    })
+);
 //
 // logger
 app.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-})
-
+    const start = new Date();
+    await next();
+    const ms = new Date() - start;
+    console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+});
+// ******************************* 体验洋葱模型 ************************
+app.use(async (ctx, next) => {
+    console.log(1);
+    await next();
+    console.log(111);
+    // await next();
+});
+app.use(async (ctx, next) => {
+    console.log(2);
+    await next();
+    console.log(222);
+    // await next();
+});
+app.use(async (ctx, next) => {
+    console.log(3);
+    await next();
+});
+// *********************************************************************
 // routes
-app.use(index.routes(), index.allowedMethods())
+app.use(index.routes(), index.allowedMethods());
 // app.use(unpmsers.routes(), users.allowedMethods())
 
 // error-handling
 app.on('error', (err, ctx) => {
-  console.error('server error', err, ctx)
+    console.error('server error', err, ctx);
 });
 
-module.exports = app
+module.exports = app;
