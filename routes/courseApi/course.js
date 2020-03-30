563891d54d6155413afb3c9b596ee11857eec309
AndySkr <542842620@qq.com>
2020-03-30 21:03:49 +0800

    删除冗余代码

--- routes/courseApi/course.js
+++
@@ -1,5 +0,0 @@
-const router = require("koa-router")();
-const CourseController = require('../../controllers/courseController')
-router.get('/lastLearn', CourseController.lastLearn)
-router.get('/courseList', CourseController.queryCourseList)
-module.exports = router
